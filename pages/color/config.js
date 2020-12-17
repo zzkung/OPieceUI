7e30f10cdf9124ad31b13e97aa5d33a77e28c310
zzkung <zzkung@outlook.com>
2020-12-17 15:06:57 +0800

    添加color config.js‘

--- pages/color/config.js
+++ pages/color/config.js
@@ -1,17 +1,124 @@
 export default {
-  mainColor: [
-    {
-      colorId: 'Red',
-      colorValue: '#fa5151'
-    }, {
-      colorId: 'Dark',
-      colorValue: '#dd6161'
-    }, {
-      colorId: 'Disabled',
-      colorValue: '#fab6b6'
-    }, {
-      colorId: 'Light',
-      colorValue: '#fef0f0'
-    }
-  ]
+  brandColor: [
+    [
+      {
+        colorId: 'Primary',
+        colorValue: '#fa5151'
+      }, {
+        colorId: 'Dark',
+        colorValue: '#dd6161'
+      }, {
+        colorId: 'Disabled',
+        colorValue: '#fab6b6'
+      }, {
+        colorId: 'Light',
+        colorValue: '#fef0f0'
+      }
+    ]
+  ],
+  functionalColor: [
+    [
+      {
+        colorId: 'Success',
+        colorValue: '#07c160'
+      }, {
+        colorId: 'Dark',
+        colorValue: '#06ae56'
+      }, {
+        colorId: 'Disabled',
+        colorValue: '#71d5a1'
+      }, {
+        colorId: 'Light',
+        colorValue: '#dbf1e1'
+      }
+    ],
+    [
+      {
+        colorId: 'Warning',
+        colorValue: '#ffc300'
+      }, {
+        colorId: 'Dark',
+        colorValue: '#e6b000'
+      }, {
+        colorId: 'Disabled',
+        colorValue: '#ffdf77'
+      }, {
+        colorId: 'Light',
+        colorValue: '#fff2c5'
+      }
+    ],
+    [
+      {
+        colorId: 'Danger',
+        colorValue: '#fa5151'
+      }, {
+        colorId: 'Dark',
+        colorValue: '#dd6161'
+      }, {
+        colorId: 'Disabled',
+        colorValue: '#fab6b6'
+      }, {
+        colorId: 'Light',
+        colorValue: '#fef0f0'
+      }
+    ],
+    [
+      {
+        colorId: 'Info',
+        colorValue: '#909399'
+      }, {
+        colorId: 'Dark',
+        colorValue: '#82848a'
+      }, {
+        colorId: 'Disabled',
+        colorValue: '#c8c9cc'
+      }, {
+        colorId: 'Light',
+        colorValue: '#f4f4f5'
+      }
+    ]
+  ],
+  fontColor: [
+    [
+      {
+        colorId: 'Primary',
+        colorValue: '#303133'
+      }, {
+        colorId: 'Regular',
+        colorValue: '#606266'
+      }, {
+        colorId: 'Secondary',
+        colorValue: '#909399'
+      }, {
+        colorId: 'Placeholder',
+        colorValue: '#c0c4cc'
+      }
+    ]
+  ],
+  borderColor: [
+    [
+      {
+        colorId: 'Base',
+        colorValue: '#dcdfe6'
+      }, {
+        colorId: 'Light',
+        colorValue: '#e4e7ed'
+      }, {
+        colorId: 'Lighter',
+        colorValue: '#ebeef5'
+      }, {
+        colorId: 'Extralight',
+        colorValue: '#f2f6fc'
+      }
+    ]
+  ],
+  backgroundColor: [
+    [
+      {
+        colorId: 'Base',
+        colorValue: '#f3f4f6'
+      }
+    ]
+  ],
+
 }
