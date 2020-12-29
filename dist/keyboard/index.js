f3541a39e7e0de8b02919fdae1955a9081ea4284
zzkung <zzkung@outlook.com>
2020-12-29 12:02:56 +0800

    完善键盘

--- dist/keyboard/index.js
+++ dist/keyboard/index.js
@@ -1,16 +1,33 @@
+import keyboardType from './config'
+
+let KEYBOARD_TYPE = {
+  digit: keyboardType.digitKeyboard,
+  english: keyboardType.englishKeyboard,
+  cartPlate: keyboardType.cartPlateKeyboard
+}
+
 Component({
   externalClasses: ['custom-class'],
   options: {
     addGlobalClass: true,
     multipleSlots: true
   },
+  observers: {
+    'keyboardType': function (value) {
+      // this.
+    }
+  },
   properties: {
     show: { type: Boolean, value: false },
     title: { type: String, value: '' },
-    zIndex: { type: Number, value: 5000, optionalTypes: [Number, String] },
     closeButtonText: { type: String, value: '' },
     closeButtonPosition: { type: String, value: 'right' },
-    borderLine: { type: Boolean, value: true }
+    borderLine: { type: Boolean, value: true },
+    keyboardType: { type: String, value: 'digit' },
+    zIndex: { type: Number, value: 5000, optionalTypes: [Number, String] }
+  },
+  data: {
+    keyboardArray: []
   },
   methods: {
     close () {
