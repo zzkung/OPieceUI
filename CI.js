1f08c03c16afbb114d1601840c491c721465dbd9
zzkung <zzkung@outlook.com>
2021-01-26 14:08:36 +0800

    edit

--- CI.js
+++ CI.js
@@ -3,26 +3,30 @@ const inquirer = require('inquirer')
 
 ;(async () => {
   let result = await inquirerResult()
-  console.log(result)
+  let buildId = randomString()
 
   const project = new ci.Project({
     appid: 'wxa1afff41f4458cd3',
     type: 'miniProgram',
-    projectPath: 'F:/CAREER/OPieceUI/miniprogram',
+    projectPath: `${process.env.PWD}/miniprogram`,
     privateKeyPath: './ci-private.key',
     ignores: ['node_modules/**/*'],
   })
+
   const previewResult = await ci.preview({
     project,
     desc: result.versionDesc, // 此备注将显示在“小程序助手”开发版列表中
     setting: {
       es6: true,
     },
+    robot: 1,
     qrcodeFormat: 'image',
-    qrcodeOutputDest: './qrcode/destination.jpg',
+    qrcodeOutputDest: `${process.env.PWD}/preview-img/preview-${buildId}.png`,
+    onProgressUpdate: console.log,
     // pagePath: 'pages/index/index', // 预览页面
     // searchQuery: 'a=1&b=2',  // 预览参数 [注意!]这里的`&`字符在命令行中应写成转义字符`\&`
   })
+  
   console.log(previewResult)
   
   function inquirerResult() {
@@ -37,5 +41,14 @@ const inquirer = require('inquirer')
         message: '写一个简单的介绍来描述这个版本的改动过:'
       }
     ])
+  }
+
+  function randomString(e) {  
+    e = e || 6;
+    var t = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678",
+    a = t.length,
+    n = "";
+    for (i = 0; i < e; i++) n += t.charAt(Math.floor(Math.random() * a));
+    return n
   }
 })()
